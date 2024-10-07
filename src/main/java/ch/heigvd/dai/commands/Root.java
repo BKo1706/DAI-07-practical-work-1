14a2a3aa2593ea6ba1d8b2eeb028bffbe26c3a78
BKo1706 <benjamin.kocher@hes-so.ch>
2024-10-07 11:54:49 +0200

    add of key parameter

--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -24,9 +24,17 @@ public class Root {
             required = true)
     protected AvailableAlgorithms algorithm;
 
+    @CommandLine.Option(
+            names = {"-k", "--key"},
+            description = "The name of the file in which the secret key used for encryption/decryption is stored",
+            required = true)
+    protected String keyfilename;
+
     public String getFilename() {return filename;}
 
     public AvailableAlgorithms getAlgorithm() {
         return algorithm;
     }
+
+    public String getKey() {return keyfilename;}
 }
