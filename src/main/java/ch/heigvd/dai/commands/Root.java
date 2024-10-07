b1c4399b7eae6dc69eac401a7371481cc4c09ed8
BKo1706 <benjamin.kocher@hes-so.ch>
2024-10-07 11:16:41 +0200

    try root

--- src/main/java/ch/heigvd/dai/commands/Root.java
+++ src/main/java/ch/heigvd/dai/commands/Root.java
@@ -1,4 +1,34 @@
 package ch.heigvd.dai.commands;
+import picocli.CommandLine;
 
+@CommandLine.Command(
+        description = "A program use for encryption and decryption of files",
+        version = "1.0.0",
+        subcommands = {
+                Encrypt.class,
+                Decrypt.class,
+        },
+        scope = CommandLine.ScopeType.INHERIT,
+        mixinStandardHelpOptions = true)
 public class Root {
+
+    public enum AvailableAlgorithms {
+        AES
+    }
+    @CommandLine.Parameters(index = "0", description = "The name of the file.")
+    protected String filename;
+
+    @CommandLine.Option(
+            names = {"-a", "--algorithm"},
+            description = "The algorithm to use (possible values: ${AES}).",
+            required = true)
+    protected AvailableAlgorithms algorithm;
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public AvailableAlgorithms getAlgorithm() {
+        return algorithm;
+    }
 }
